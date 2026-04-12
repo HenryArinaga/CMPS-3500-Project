50bd2d318fce102d9c9678dde7ed1988e1c8b304
Henry Arinaga <henry.arinaga@icloud.com>
2026-04-11 17:08:47 -0700

    fix: output string in main

--- CMPS3500_MiniScheme_Starter_Package/procedural/main.cpp
+++ CMPS3500_MiniScheme_Starter_Package/procedural/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cout << "Usage: ./miniscm <file_name>\n";
+        std::cout << "Usage: ./test <file_name>\n";
         return 1;
     }
 
