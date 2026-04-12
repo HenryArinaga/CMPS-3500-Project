9a98d9a0c0991f81d2e0c7725e1e936f5fa7ef2d
albert-at-csu <albertocar201061@icloud.com>
2026-04-12 04:24:56 -0700

    change run_all.sh and program output
    I also moved a couple of files around. I'm using test-script.sh for now, once I get it working right I will copy its contents into run_all.sh. The name of the executable is hard-coded into the script (test-script.sh), so change that if you want to test it with your program :)

--- CMPS3500_MiniScheme_Starter_Package/run_all.sh
+++ CMPS3500_MiniScheme_Starter_Package/run_all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# variable cmd is set to the first argument passed
 cmd="${1:-}"
 case "$cmd" in
   list-cases)
@@ -9,10 +10,18 @@ case "$cmd" in
   run-case)
     impl="${2:-}"
     file="${3:-}"
+    # if impl=procedural:
+    if [ $impl -eq "procedural" ]; then
+    # run: ./cplusplus <file>
+    output=$(./"program-name" "$file")
     echo "Implementation: ${impl}"
     echo "Case: ${file}"
-    echo "Status: ERROR"
-    echo "Error: NOT_IMPLEMENTED"
+    # Status is always OK for now
+    echo "Status: OK"
+    echo "Result: ${output}"
+    # CHANGE TYPE, maybe use a variable.
+    echo "Type: it-varies"
+    #echo "Error: NOT_IMPLEMENTED"
     ;;
   compare-case)
     file="${2:-}"
@@ -20,7 +29,7 @@ case "$cmd" in
     echo
     echo "procedural: ERROR -> NOT_IMPLEMENTED"
     echo "oop:        ERROR -> NOT_IMPLEMENTED"
-    echo "functional: ERROR -> NOT_IMPLEMENTED"
+    #echo "functional: ERROR -> NOT_IMPLEMENTED"
     ;;
   *)
     echo "Usage: ./run_all.sh {list-cases|run-case <implementation> <file>|compare-case <file>}"
