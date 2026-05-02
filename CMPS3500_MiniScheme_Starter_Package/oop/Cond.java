edaf62183d437c0da1be7354dfe4e0b5ec617ad9
Peter <97195679+Small321@users.noreply.github.com>
2026-05-01 22:10:17 -0700

    add labels to top of cond and recursion

--- CMPS3500_MiniScheme_Starter_Package/oop/Cond.java
+++ CMPS3500_MiniScheme_Starter_Package/oop/Cond.java
@@ -1,3 +1,11 @@
+/*
+  / NAME: Henry Arinaga, Alberto Molina, Peter Uzuriaga /
+  / ASGT: CHECKPOINT 3                                  /
+  / ORGN: CSUB - CMPS 3500                              /
+  / FILE: Cond.java                                     /
+  / DATE: 05/01/2026                                    /
+*/
+
 import java.util.List;
 import java.util.ArrayList;
 
--- CMPS3500_MiniScheme_Starter_Package/oop/Recursion.java
+++ CMPS3500_MiniScheme_Starter_Package/oop/Recursion.java
@@ -1,3 +1,13 @@
+/*
+  / NAME: Henry Arinaga, Alberto Molina, Peter Uzuriaga /
+  / ASGT: CHECKPOINT 3                                  /
+  / ORGN: CSUB - CMPS 3500                              /
+  / FILE: Recursion.java                                /
+  / DATE: 05/01/2026                                    /
+*/
+
+import java.util.List;
+
 public class Recursion {
     
 }
