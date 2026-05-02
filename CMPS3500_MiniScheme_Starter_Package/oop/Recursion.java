77bb41d8789e0fcb59740ed7923472d89f9f29c6
Peter <97195679+Small321@users.noreply.github.com>
2026-05-01 22:16:17 -0700

    Implement Recursion.java

--- CMPS3500_MiniScheme_Starter_Package/oop/Recursion.java
+++ CMPS3500_MiniScheme_Starter_Package/oop/Recursion.java
@@ -9,5 +9,20 @@
 import java.util.List;
 
 public class Recursion {
-    
+    public static boolean isRecursiveDefinition(List<String> valueExpr){
+        return !valueExpr.isEmpty() &&
+        valueExpr.get(0).equals("(") &&
+        valueExpr.size() > 1 &&
+        valueExpr.get(1) == "lambda";
+    }
+
+    public static String handleRecursiveDefine(
+        String name,
+        List<String> valueExpr,
+        Scope scope
+    ){
+        String value = Evaluate.evaluate(valueExpr, scope);
+        scope.addScopeEntry(name, value);
+        return "";
+    }
 }
