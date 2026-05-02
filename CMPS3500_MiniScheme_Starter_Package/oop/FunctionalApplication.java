807060c31e0276385d0c783e3dfc451fd1ad8c19
Peter <97195679+Small321@users.noreply.github.com>
2026-05-01 22:17:42 -0700

    Create FunctionalApplication.java

---
+++ CMPS3500_MiniScheme_Starter_Package/oop/FunctionalApplication.java
@@ -0,0 +1,13 @@
+/*
+  / NAME: Henry Arinaga, Alberto Molina, Peter Uzuriaga /
+  / ASGT: CHECKPOINT 3                                  /
+  / ORGN: CSUB - CMPS 3500                              /
+  / FILE: FunctionalApplication.java                    /
+  / DATE: 05/01/2026                                    /
+*/
+
+import java.util.List;
+
+public class FunctionalApplication {
+    
+}
