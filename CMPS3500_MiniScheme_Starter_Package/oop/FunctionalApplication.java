e9a3e79a34d632cbe96e612dbf2871082554b361
Peter <97195679+Small321@users.noreply.github.com>
2026-05-01 23:57:28 -0700

    Partial FunctionalApplication.java Implementation

--- CMPS3500_MiniScheme_Starter_Package/oop/FunctionalApplication.java
+++ CMPS3500_MiniScheme_Starter_Package/oop/FunctionalApplication.java
@@ -7,7 +7,184 @@
 */
 
 import java.util.List;
+import java.util.ArrayList;
 
-public class FunctionalApplication {
-    
+public class FunctionalApplication extends ExpressionHandler {
+    public FunctionalApplication(List<String> expression){
+        super(expression, 1);
+    }
+
+    private static boolean isIntegerLiteral(String token){
+        if(token.isEmpty()){
+            return false;
+        }
+
+        int start = 0;
+
+        if(token.charAt(0) == '-'){
+            if(token.length() == 1){
+                return false;
+            }
+
+            start = 1;
+        }
+
+        for(int i = start; i < token.length(); i++){
+            if(!Character.isDigit(token.charAt(i))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean resolveValue(
+        String token,
+        Scope scope,
+        int result,
+        String error
+    ){
+        String value = token;
+
+        if(!isIntegerLiteral(value)){
+            if(value.equals("#t") || value.equals("#f")){
+                error = "TYPE_MISMATCH";
+                return false;
+            }
+
+            value = scope.lookupScopeEntry(value);
+
+            if(value == "NOT FOUND"){
+                error = "UNDECLARED_IDENTIFIER";
+                return false;
+            }
+
+            if(!isIntegerLiteral(value)){
+                error = "TYPE_MISMATCH";
+                return false;
+            }
+        }
+
+        result = Integer.parseInt(value);
+        return true;
+    }
+
+    private boolean resolveExpressionValue(
+        Scope scope,
+        int result,
+        String error
+    ){
+        if(!expression.get(index).equals("(")){
+            if(!resolveValue(expression.get(index), scope, result, error)){
+                return false;
+            }
+
+            index++;
+            return true;
+        }
+
+        List<String> nestedExpression = extractPart();
+
+        String value = Evaluate.evaluate(nestedExpression, scope);
+        
+        if(getError(value)){
+            error = value;
+            return false;
+        }
+
+        if(!isIntegerLiteral(value)){
+            error = "TYPE_MISMATCH";
+            return false;
+        }
+
+        result = Integer.parseInt(value);
+        return true;
+    }
+
+    // public static String handleFunctionApplication(
+    //     List<String> expression,
+    //     Scope scope
+    // ){
+    //     String op = expression.get(0);
+
+    //     if(op.equals("(")){
+    //         FunctionalApplication funcAppExpression = new FunctionalApplication(expression);
+    //         funcAppExpression.index = 0;
+    //         List<String> lambdaExpr = funcAppExpression.extractPart();
+    //         List<List<String>> arguments = new ArrayList<>();
+
+    //         while(funcAppExpression.index < expression.size()){
+    //             arguments.add(funcAppExpression.extractPart());
+    //         }
+
+    //         String lambdaValue = Evaluate.evaluate(lambdaExpr, scope);
+
+    //         if(getError(lambdaValue)){
+    //             return lambdaValue;
+    //         }
+
+    //         return Lambda.applyLambdaValue(lambdaValue, arguments, scope);
+    //     }
+
+    //     String lambdaValue = scope.lookupScopeEntry(op);
+
+    //     if(Lambda.isLambdaValue(lambdaValue)){
+    //         FunctionalApplication funcAppExpression = new FunctionalApplication(expression);
+    //         funcAppExpression.index = 1;
+    //         List<List<String>> arguments = new ArrayList<>();
+
+    //         while(funcAppExpression.index < expression.size()){
+    //             arguments.add(funcAppExpression.extractPart());
+    //         }
+
+    //         return Lambda.applyLambdaValue(lambdaValue, arguments, scope);
+    //     }
+
+    //     if (op.equals("+")) {
+    //         int result = 0;
+    //         FunctionalApplication funcAppExpression = new FunctionalApplication(expression);
+    //         funcAppExpression.index = 1;
+
+    //         while (funcAppExpression.index < expression.size()) {
+    //             int value = 0;
+    //             String error = "";
+
+    //             if (!funcAppExpression.resolveExpressionValue( scope, value, error)) {
+    //                 return error;
+    //             }
+
+    //             result += value;
+    //         }
+
+    //         return Integer.toString(result);
+    //     } 
+    //     else if (op.equals("-")) {
+    //         if (expression.size() < 2) {
+    //             return "WRONG_ARITY";
+    //         }
+
+    //         int result = 0;
+    //         FunctionalApplication funcAppExpression = new FunctionalApplication(expression);
+    //         funcAppExpression.index = 1;
+    //         String error = "";
+
+    //         if (!funcAppExpression.resolveExpressionValue( scope, result, error)) {
+    //             return error;
+    //         }
+
+    //         while(funcAppExpression.index < expression.size()){
+    //         int value = 0;
+
+    //             if (!funcAppExpression.resolveExpressionValue( scope, value, error)) {
+    //                 return error;
+    //             }
+                
+    //             result -= value;
+    //         }
+
+    //         return 
+    //     }
+    // }
+        
 }
+    
--- CMPS3500_MiniScheme_Starter_Package/oop/Main.java
+++ CMPS3500_MiniScheme_Starter_Package/oop/Main.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class Main{
     public static void main(String[] args) throws Exception {
         if(args.length != 1){
-            System.out.println("Usage: ./miniscm <file_name>");
             return;
         }
 
